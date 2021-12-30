ce8badb8cbc97d642d056c9b0636f0c5fc87ac4a
cyebukayire <peacecyebukayire@gmail.com>
2021-12-30 19:22:08 +0200

    Added: watches, computed, methods & mounted

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
-<div></div>
+<div id="app">
+  @{{ user.username }} - {{fullName}}
+  <strong>Followers: </strong> {{followers}}
+  <button @click="followUser">Follow</button>
+</div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'Peace',
+        firstName: 'Peace',
+        lastName: 'Aida',
+        email: 'peace@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`@${this.user.username} has gained a follower`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    }
+  },
+  // Runs the functions before the first time to be called
+  mounted() {
+    this.followUser();
+  }
 }
 </script>
 
@@ -16,6 +54,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
